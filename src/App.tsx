f02788a50270eca5bfcb96e660dd014e083f5f17
Gabriel <gabrielxleal@gmail.com>
2020-10-13 18:29:30 -0300

    landing page criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import './styles/global.css'; //Todas importações de CSS ou Imagem, vão partir sempre de arquivos tsx ou jsx e não pelo HTML 
+import './styles/pages/landing.css';
+import logoImg from './images/logo.svg';
+import {FiArrowRight} from 'react-icons/fi'
 
-
-
-function App() {
+//o App.tsx é um  react component
+function App() { // componente no react -> função que retorna um conteúdo HTML ou JSX
   return (
-    <div className="App">
-      <h1>hello oi</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Fortaleza</strong>
+          <span>Ceará</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight  size={26} color="rgba(0, 0, 0.6)" />
+        </a>
+
+
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//primeiro arquivo JS carregado carregado pela aplicação
+//extensão .tsx ou .jsx -> JavaScript ou TypeScript xml , permite HTML dentro do JS
+import React from 'react'; // importação necessária para todo arquivo que for utilizar da sintaxe de HTML dentro do arquivo
+import ReactDOM from 'react-dom'; //integração do REACT com a DOM 
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.render( // metodo render recebe tags html ou componentes(funções que retornam jsx html)
   <React.StrictMode>
     <App />
   </React.StrictMode>,
