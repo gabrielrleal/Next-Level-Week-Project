2828717e268463148073b4b2f8f1400ce3820051
Gabriel <gabrielxleal@gmail.com>
2020-10-16 18:03:59 -0300

    abstraindo sidebar em component

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanage-map.css'
-import { Map, TileLayer } from 'react-leaflet'
+
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+
+import Leaflet from 'leaflet';
+
 
 import 'leaflet/dist/leaflet.css'
+
+const mapIcon = Leaflet.icon({ 
+    iconUrl: mapMarkerImg, 
+
+    iconSize: [58, 68], //tamanho
+    iconAnchor: [29, 68], //posição
+    popupAnchor: [178, 2] //posição do popup
+})
+
+
+
 
 function OrphanagesMap() {
     return (
         <div id="page-map">
             <aside>
                 <header>
-                    <img src={mapMarkerImg} />
+                    <img src={mapMarkerImg} alt="mapMarker" />
                     <h2>Escolha um orfanato no mapa</h2>
                     <p>Muitas crianças estão esperando a sua visita! </p>
                 </header>
@@ -22,18 +37,32 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <Map 
-                center={[-3.7440853,-38.4955035]}
+            <Map
+                center={[-3.7440853, -38.4955035]}
                 zoom={15}
-                style={{width:'100%', height:'100%'}} >
-                    <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                style={{ width: '100%', height: '100%' }} >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+
+                <Marker
+                    icon={mapIcon} alt ="mapMarker" 
+                    position={[-3.7440853, -38.4955035]}>
+                        <Popup closeButton = {false} minWidth={240} maxWidth={240} className="map-popup">
+                            nome do orfanato
+                            <Link to ="/orphanages/1">
+                                <FiArrowRight size={20} color="#FFF"/>
+                            </Link>
+                        </Popup>
+                    </Marker>
+
+
             </Map>
 
-            <Link to = "" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
-    );
+    )
 }
 
 
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 //switch faz com que apenas uma rota seja exibida em tela
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
-
+import CreateOrphanage from './pages/CreateOrphanage';
+import Orphanage from './pages/Orphanage';
 function Routes(){ // exact comparação de igualdade, geralmente colocada na primeira rota "/" 
 
     return(        
@@ -11,6 +12,10 @@ function Routes(){ // exact comparação de igualdade, geralmente colocada na pr
             <Switch> 
                 <Route path="/" exact component= {Landing} /> 
                 <Route path="/app" component={OrphanagesMap} />
+                <Route path="/orphanages/create" component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} />
+                
+                
             </Switch>   
         </BrowserRouter>
     );
