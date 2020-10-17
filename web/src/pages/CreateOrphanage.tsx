aba8fbe968b693462f76fb8a243ae7bab5493270
Gabriel <gabrielxleal@gmail.com>
2020-10-17 19:21:17 -0300

    correção de bugs e detalhamento de orfanatos

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,56 +1,56 @@
-import React from "react";
-import { Map, Marker, TileLayer } from 'react-leaflet';
-import {  FiPlus } from "react-icons/fi";
+import React from 'react'
+import { Map, Marker, TileLayer } from 'react-leaflet'
+import { FiPlus } from 'react-icons/fi'
 
-import '../styles/pages/create-orphanage.css';
-import Sidebar from "../components/Sidebar";
-import mapIcon from "../utils/mapIcon";
+import '../styles/pages/create-orphanage.css'
+import Sidebar from '../components/Sidebar'
+import mapIcon from '../utils/mapIcon'
 
-
-export default function CreateOrphanage() {
-  
-
+export default function CreateOrphanage () {
   return (
-    <div id="page-create-orphanage">
-
-      <Sidebar/>
+    <div id='page-create-orphanage'>
+      <Sidebar />
 
       <main>
-        <form className="create-orphanage-form">
+        <form className='create-orphanage-form'>
           <fieldset>
             <legend>Dados</legend>
 
-            <Map 
-              center={[-27.2092052,-49.6401092]} 
+            <Map
+              center={[-27.2092052, -49.6401092]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
             >
-              <TileLayer 
+              <TileLayer
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} />
+              <Marker
+                interactive={false}
+                icon={mapIcon}
+                position={[-27.2092052, -49.6401092]}
+              />
             </Map>
 
-            <div className="input-block">
-              <label htmlFor="name">Nome</label>
-              <input id="name" />
+            <div className='input-block'>
+              <label htmlFor='name'>Nome</label>
+              <input id='name' />
             </div>
 
-            <div className="input-block">
-              <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
-              <textarea id="name" maxLength={300} />
+            <div className='input-block'>
+              <label htmlFor='about'>
+                Sobre <span>Máximo de 300 caracteres</span>
+              </label>
+              <textarea id='name' maxLength={300} />
             </div>
 
-            <div className="input-block">
-              <label htmlFor="images">Fotos</label>
+            <div className='input-block'>
+              <label htmlFor='images'>Fotos</label>
 
-              <div className="uploaded-image">
+              <div className='uploaded-image'></div>
 
-              </div>
-
-              <button className="new-image">
-                <FiPlus size={24} color="#15b6d6" />
+              <button className='new-image'>
+                <FiPlus size={24} color='#15b6d6' />
               </button>
             </div>
           </fieldset>
@@ -58,33 +58,35 @@ export default function CreateOrphanage() {
           <fieldset>
             <legend>Visitação</legend>
 
-            <div className="input-block">
-              <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
+            <div className='input-block'>
+              <label htmlFor='instructions'>Instruções</label>
+              <textarea id='instructions' />
             </div>
 
-            <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
-              <input id="opening_hours" />
+            <div className='input-block'>
+              <label htmlFor='opening_hours'>Nome</label>
+              <input id='opening_hours' />
             </div>
 
-            <div className="input-block">
-              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+            <div className='input-block'>
+              <label htmlFor='open_on_weekends'>Atende fim de semana</label>
 
-              <div className="button-select">
-                <button type="button" className="active">Sim</button>
-                <button type="button">Não</button>
+              <div className='button-select'>
+                <button type='button' className='active'>
+                  Sim
+                </button>
+                <button type='button'>Não</button>
               </div>
             </div>
           </fieldset>
 
-          <button className="confirm-button" type="submit">
+          <button className='confirm-button' type='submit'>
             Confirmar
           </button>
         </form>
       </main>
     </div>
-  );
+  )
 }
 
 // return `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,90 +1,94 @@
-import React, { useEffect, useState } from 'react';
-import { FiPlus, FiArrowRight } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
-import mapMarkerImg from '../images/map-marker.svg';
-import '../styles/pages/orphanage-map.css'
-import api from '../services/api';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { useEffect, useState } from "react";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import mapMarkerImg from "../images/map-marker.svg";
+import "../styles/pages/orphanage-map.css";
+import api from "../services/api";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
-import Leaflet from 'leaflet';
+import Leaflet from "leaflet";
 
-
-import 'leaflet/dist/leaflet.css'
-
+import "leaflet/dist/leaflet.css";
 
 const mapIcon = Leaflet.icon({
-    iconUrl: mapMarkerImg,
+  iconUrl: mapMarkerImg,
 
-    iconSize: [58, 68], //tamanho
-    iconAnchor: [29, 68], //posição
-    popupAnchor: [178, 2] //posição do popup
-})
+  iconSize: [58, 68], //tamanho
+  iconAnchor: [29, 68], //posição
+  popupAnchor: [178, 2], //posição do popup
+});
 
-interface Orphanage{ // interface com quais informações de orphanage iremos utilizar nesta tela 
-    id: number;
-    latitude: number;
-    longitude: number; 
-    name: string;
+interface Orphanage {
+  // interface com quais informações de orphanage iremos utilizar nesta tela
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
 }
-
 
 function OrphanagesMap() {
-    //parametros: array de orfanatos , função para atualizar valores do array
-    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);//cria array vazio tipado como um array do tipo Orphanage (interface que contem as informações que serão utilizadas)
-    //parametros: qual ação quero executar, quando executar ação
-    useEffect(() => {
-        api.get('orphanages').then(response => {//buscou orphanages na api
-            setOrphanages(response.data);//quando for atualizar usa setOrphanages, preenche os orphanages no array
-        })
-    }, []);
+  //parametros: array de orfanatos , função para atualizar valores do array
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]); //cria array vazio tipado como um array do tipo Orphanage (interface que contem as informações que serão utilizadas)
+  //parametros: qual ação quero executar, quando executar ação
+  useEffect(() => {
+    api.get("orphanages").then((response) => {
+      //buscou orphanages na api
+      setOrphanages(response.data); //quando for atualizar usa setOrphanages, preenche os orphanages no array
+    });
+  }, []);
 
-    return (
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="mapMarker" />
-                    <h2>Escolha um orfanato no mapa</h2>
-                    <p>Muitas crianças estão esperando a sua visita! </p>
-                </header>
-                <footer>
-                    <strong> Fortaleza</strong>
-                    <span> Ceará</span>
-                </footer>
-            </aside>
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="mapMarker" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita! </p>
+        </header>
+        <footer>
+          <strong> Fortaleza</strong>
+          <span> Ceará</span>
+        </footer>
+      </aside>
 
-            <Map
-                center={[-3.7440853, -38.4955035]}
-                zoom={15}
-                style={{ width: '100%', height: '100%' }} >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Map
+        center={[-3.7440853, -38.4955035]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-                
-                {orphanages.map(orphanage => {//por que não for each? porque o map percorre vai array e retorna algo
-                    return (//não é identificada oque tem dentro do orphanage, pois não se sabe qual o formato de orphanage, para isso, é preciso criar uma interface com as informações de orphanage irão ser utilizadas
-                        <Marker
-                            key={orphanage.id}
-                            icon={mapIcon} alt="mapMarker"
-                            position={[orphanage.latitude, orphanage.longitude]}> 
-                            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-                                {orphanage.name}
-                                <Link to={`/orphanages/${orphanage.id}`}>
-                                    <FiArrowRight size={20} color="#FFF" />
-                                </Link>
-                            </Popup>
-                        </Marker>
-                    )
-                })}
+        {orphanages.map((orphanage) => {
+          //por que não for each? porque o map percorre vai array e retorna algo
+          return (
+            //não é identificada oque tem dentro do orphanage, pois não se sabe qual o formato de orphanage, para isso, é preciso criar uma interface com as informações de orphanage irão ser utilizadas
+            <Marker
+              key={orphanage.id}
+              icon={mapIcon}
+              alt="mapMarker"
+              position={[orphanage.latitude, orphanage.longitude]}
+            >
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+                className="map-popup"
+              >
+                {orphanage.name}
+                <Link to={`/orphanages/${orphanage.id}`}>
+                  <FiArrowRight size={20} color="#FFF" />
+                </Link>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </Map>
 
-
-            </Map>
-
-            <Link to="/orphanages/create" className="create-orphanage">
-                <FiPlus size={32} color="#FFF" />
-            </Link>
-        </div>
-    )
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
 }
-
-
 
 export default OrphanagesMap;
